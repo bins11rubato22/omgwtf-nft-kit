513a0a42bf6c22102d1435960051d294efee0d20
bins11rubato22 <164144136+bins11rubato22@users.noreply.github.com>
2024-03-20 08:11:33 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# omgwtf-nft-kit
+
+Explore NFTs and DeFi points with a comprehensive kit for NFT and DeFi interactions.
+
+## Installation
+
+```bash
+git clone https://github.com/bins11rubato22/omgwtf-nft-kit.git
+cd omgwtf-nft-kit
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,110 @@
+const omgwtf_nft_kit = require('omgwtf-nft-kit');
+const omgwtf_defi_points_kit = require('omgwtf-defi-points-kit');
+const body_parser = require('body-parser');
+const jsonwebtoken = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const web3 = require('web3');
+const ipfs_http_client = require('ipfs-http-client');
+const request = require('request');
+const pg = require('pg');
+const eth_crypto = require('eth-crypto');
+const sequelize = require('sequelize');
+const ejs = require('ejs');
+const webpack = require('webpack');
+const sinon = require('sinon');
+const bluebird = require('bluebird');
+const dotenv = require('dotenv');
+const ethereumjs_tx = require('ethereumjs-tx');
+const mysql = require('mysql');
+const ganache_cli = require('ganache-cli');
+const socket.io = require('socket.io');
+const fs_extra = require('fs-extra');
+const react_dom = require('react-dom');
+
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+server.on('message', (msg, rinfo) => {
+  console.log(`Message from client: ${msg.toString()}`);
+  console.log(`Client info: ${rinfo.address}:${rinfo.port}`);
+});
+server.bind(8080);
+
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+if (isMainThread) {
+  const worker = new Worker(__filename, { workerData: 'Hello from main thread' });
+  worker.on('message', message => {
+    console.log('Received message from worker:', message);
+  });
+} else {
+  parentPort.postMessage('Hello from worker thread');
+}
+
+// Get the balance of an Ethereum address
+web3.eth.getBalance('0x1234567890123456789012345678901234567890').then(balance => {
+  console.log('Balance:', web3.utils.fromWei(balance, 'ether'), 'ETH');
+}).catch(err => {
+  console.error('Error getting balance:', err);
+});
+
+const assert = require('assert');
+assert.strictEqual(1, 1, '1 is strictly equal to 1');
+
+class Polygon {
+  constructor(height, width) {
+    this.area = height * width;
+  }
+}
+console.log(new Polygon(4, 3).area);
+
+const { Transform } = require('stream');
+const upperCaseTransform = new Transform({
+  transform(chunk, encoding, callback) {
+    this.push(chunk.toString().toUpperCase());
+    callback();
+  }
+});
+process.stdin.pipe(upperCaseTransform).pipe(process.stdout);
+
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
+eventEmitter.on('customEvent', (message) => {
+  console.log(`Custom Event Received: ${message}`);
+});
+eventEmitter.emit('customEvent', 'Hello EventEmitter');
+
+// Verify a message signature
+const signerAddress = web3.eth.accounts.recover(messageHash, signature.signature);
+console.log('Signer address:', signerAddress);
+
+// Convert a string to title case
+function toTitleCase(str) {
+  return str.toLowerCase().replace(/\b\w/g, c => c.toUpperCase());
+}
+console.log('Title case:', toTitleCase('hello world'));
+
+// Interact with a deployed smart contract using Hardhat
+async function interactWithContract() {
+  const contractAddress = '0xcontractAddress';
+  const YourContract = await ethers.getContractFactory('YourContract');
+  const contract = await YourContract.attach(contractAddress);
+
+  const result = await contract.someMethod();
+  console.log('Method result:', result);
+}
+
+interactWithContract().catch(err => {
+  console.error('Error interacting with contract:', err);
+});
+
+const http = require('http');
+http.get('http://example.com', (resp) => {
+  let data = '';
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+  resp.on('end', () => {
+    console.log(data);
+  });
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
